d6d378c0e6b12e9de0c09829788c225374e1efb1
your Name <mohamedabdulayubm@gmail.com>
2022-06-25 13:51:00 +0530

    Check attd

--- EmployeeWage_OOPS/Program.cs
+++ EmployeeWage_OOPS/Program.cs
@@ -1,1 +1,17 @@
 ﻿Console.WriteLine("Welcome to Employee Wage Computation");
+int empPresent = 1;
+Random num = new Random();
+int attendance = num.Next(0, 2);
+if (attendance == empPresent)
+{
+
+
+    Console.WriteLine("Is Present");
+
+
+}
+
+else
+{
+    Console.WriteLine("Is Absent");
+}
