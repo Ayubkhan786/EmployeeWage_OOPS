04390293fa3714262fb89507fed86c29f3382db3
your Name <mohamedabdulayubm@gmail.com>
2022-06-25 16:13:05 +0530

    Multi Company Wages

--- EmployeeWage_OOPS/Program.cs
+++ EmployeeWage_OOPS/Program.cs
@@ -7,13 +7,20 @@ using EmployeeWage_OOPS;
  
 
 
-//Console.WriteLine("Welcome to Employee Wage Computation");
 
 
 
-    EmpWage empWage = new EmpWage();
-empWage.EmployeeWage("TCS", 30, 28, 56); 
-empWage.EmployeeWage("CTS", 40, 20, 70); 
-empWage.EmployeeWage("INFOSYS", 50, 22, 60); 
-empWage.EmployeeWage("CAPGEMINI", 65, 25, 40); 
+    EmpWage empWageBuilder = new EmpWage();
+empWageBuilder.EmployeeWage("TCS", 30, 28, 56);
 
+
+EmpWage empWageBuilder1 = new EmpWage();
+empWageBuilder1.EmployeeWage("CTS", 40, 20, 70);
+
+
+EmpWage empWageBuilder2 = new EmpWage();
+empWageBuilder2.EmployeeWage("CTS", 40, 20, 70);
+
+
+EmpWage empWageBuilder3 = new EmpWage();
+empWageBuilder.EmployeeWage("CTS", 40, 20, 70);
