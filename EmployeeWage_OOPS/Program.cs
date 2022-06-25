fdaace6487318788bf2455ef731892fa0ad68ce2
your Name <mohamedabdulayubm@gmail.com>
2022-06-25 13:53:28 +0530

    Wage

--- EmployeeWage_OOPS/Program.cs
+++ EmployeeWage_OOPS/Program.cs
@@ -1,17 +1,22 @@
 ﻿Console.WriteLine("Welcome to Employee Wage Computation");
 int empPresent = 1;
+int empWrkHrs = 0;
+int empSalary = 50;
 Random num = new Random();
 int attendance = num.Next(0, 2);
 if (attendance == empPresent)
 {
-
-
     Console.WriteLine("Is Present");
-
-
+    empWrkHrs = 8;
+    empSalary = 50;
 }
 
 else
 {
     Console.WriteLine("Is Absent");
+    empWrkHrs = 0;
+    empSalary = 0;
 }
+
+int empWage = (empWrkHrs * empSalary);
+Console.WriteLine("The total salary is :" + empWage);
